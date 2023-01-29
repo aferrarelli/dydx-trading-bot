868148005d2c04a88fdb52be6e55d2475561d312
Andrea Ferrarelli <andrea.ferrarelli.94@gmail.com>
2023-01-29 09:37:01 +0100

    stage 2 start

--- program/main.py
+++ program/main.py
@@ -3,12 +3,9 @@ from func_connections import connect_dydx
 from func_private import abort_all_positions
 from func_public import construct_market_prices
 from func_messaging import send_message
+from func_cointegration import store_cointegration_results
 
 if __name__ == "__main__":
-    
-    # success = send_message("Wowzers another awesome message")
-    # print(success)
-    exit(1)
 
     # Connect to client
     try:
@@ -29,9 +26,22 @@ if __name__ == "__main__":
 
     # Find Cointegrated Pairs
     if FIND_COINTEGRATED:
+
+        # Construct Market Prices
         try:
             print("Fecthing market prices, please allow 3 mins...")
             df_market_prices = construct_market_prices(client)
         except Exception as e:
             print("Error constructing market prices: ", e)
+            exit(1)
+
+        # Store Cointegrated Pairs
+        try:
+            print("Storing cointegrated pairs...")
+            stores_result = store_cointegration_results(df_market_prices)
+            if stores_result != "saved":
+                print("Error saving cointegrated pairs")
+                exit(1)
+        except Exception as e:
+            print("Error saving cointegration pairs: ", e)
             exit(1)
